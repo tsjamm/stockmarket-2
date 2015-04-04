0d28f6e3e06c6a594d6ea7f725983cf1e30e9064
Antonio Leon <antonio.leon.alcaide@gmail.com>
2015-04-04 15:14:06 +0200

    Add filters to the basic search

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var
 	express = require('express'),
 	app = express(),
 	consolidate = require('consolidate'),
-	routes = require('./controllers/routes');
+	routes = require('./controllers/routes'),
+	bodyParser = require('body-parser');
 
 	app.set('view engine', 'ejs');
 	app.set('views', __dirname + '/views');
 	app.use(express.static(__dirname + '/'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
 	app.listen(3000);
 
 	routes(app);
+	console.log('Server listening at 3000');
--- controllers/main_routes.js
+++ controllers/main_routes.js
@@ -1,7 +1,56 @@
+var getData = require('./../public/js/getData');
+var request = require('request');
 
 var route = function(app) {
 	app.get('/',function(req,res) {
-		res.render(__dirname + './../views/home');
+		res.render(__dirname + './../views/home',{ data:''});
+	});
+
+	app.post('/home', function(req,res) {
+		function getData(cb) {
+			var data = '';
+			var table = req.body.tableSearch;
+			var data_format = '.json';
+			var sort_order = req.body.orderSearch;
+			var trim_start = req.body.trim_startSearch;
+			var trim_end = req.body.trim_endSearch;
+			var collapse = req.body.collapseSearch;
+
+
+			var url = 'http://www.quandl.com/api/v1/datasets/WIKI/' + table + data_format + '?sort_order='+sort_order+'&trim_start='+trim_start+'&trim_end='+trim_end+'&collapse='+collapse;
+
+
+			request({url: url, json: true}, function(err,response,body) {
+				console.log(body);
+				data+= '<table class="table"> <tr>';
+				for(var column in body.column_names){
+					data += '<td>' + body.column_names[column]+ '</td>';
+					if(body.column_names[column]=='Volume')
+						break;
+				}
+
+				data+='</tr>';
+
+				for(var row in body.data) {
+					data +='<tr>';
+					for(var i=0; i<6; i++){
+						if(body.column_names[i]=='Date')
+							data +='<td>'+ body.data[row][i] + '</td>';
+						else
+							data +='<td>'+ Number(body.data[row][i]).toFixed(2) + '</td>';
+					}
+					data +='</tr>';
+				
+				}
+
+				data +='</table>';
+				cb(data);
+			});	
+		}
+
+		getData(function(data) {
+			res.render(__dirname + './../views/home', { data: data } );
+		});
 	});
 
 	app.get('/about',function(req,res) {
