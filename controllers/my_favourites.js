ea575dc7a47fee7245daab61dd69894ff9f450ed
aleonalcaide <antonio.leon.alcaide@gmail.com>
2015-05-23 02:15:59 +0200

    Displaying favourites without charts and remove option

--- controllers/my_favourites.js
+++ controllers/my_favourites.js
@@ -1,12 +1,23 @@
+var UserSchema = require('./../models/UserSchema');
+var User = new UserSchema();
+
 var route = function(app) {
 
 	app.get('/my_favourites',function(req,res) {
-		if(req.session && req.session.email)
-			res.render(__dirname + './../views/my_favourites', {
-				username: req.session.username,
-				twitterWidget1: req.session.twitterWidget1,
-				twitterWidget2: req.session.twitterWidget2
+		if(req.session && req.session.email) {
+
+
+			UserSchema.findOne( {email: req.session.email}, function(err,user) {
+				if(err) return console.log('Error reading user data : ' + err);
+
+				res.render(__dirname + './../views/my_favourites', {
+					username: req.session.username,
+					twitterWidget1: req.session.twitterWidget1,
+					twitterWidget2: req.session.twitterWidget2,
+					favourites: user.favourites
+				});
 			});
+		}
 		else
 			res.redirect('/');
 	});
